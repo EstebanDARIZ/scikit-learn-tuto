3e52ee37b5444a47b679bf65bad3fdc7403ff88c
EstebanDreauDariz <edreau@bordeaux-inp.fr>
2025-08-27 16:37:40 +0200

    Write the begining of the code tuto_partie_1
    Training a model to predict the cost of a house, r2 = 60%

--- tuto_partie_1.py
+++ tuto_partie_1.py
@@ -1,2 +1,39 @@
-from sklearn.datasets import load_boston
+from sklearn import datasets 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+housing = datasets.fetch_california_housing()
+
+x = housing.data
+y = housing.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 432)  # test_size = 0.2, on garde 20% du dataset pour le test 
+
+model = LinearRegression()
+
+model.fit(x_train, y_train)   # training 
+
+y_pred = model.predict(x_test)  #testing 
+r2 = r2_score(y_test, y_pred)
+
+
+# print(housing.feature_names)
+# print(x[0])
+# print(y[0])
+print(r2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
